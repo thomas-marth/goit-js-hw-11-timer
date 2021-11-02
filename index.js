ddebe05f748fc3f5f2e6f8d7d9da5fdf76df619f
Dmitriy Bazhenov <dmitriy.bazhenov@gmail.com>
2021-11-02 20:38:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,10 +24,12 @@
 // };
 
 // function updateClockface(time) {
-//   const days = Math.floor(time / (1000 * 60 * 60 * 24));
-//   const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-//   const secs = Math.floor((time % (1000 * 60)) / 1000);
+//   const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+//   const hours = pad(
+//     Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+//   );
+//   const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+//   const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
 
 //   refs.days.textContent = `${days}`;
 //   refs.hours.textContent = `${hours}`;
